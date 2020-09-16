e23919e21e440572f21a85a8bbf87c0bf39b6d85
hongyue-qiu <994059641@qq.com>
2020-09-16 21:31:52 +0800

    [qiuhongyue] get user list is finish

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -1,11 +1,10 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.dto.RsEvent;
 import com.thoughtworks.rslist.dto.User;
 import com.thoughtworks.rslist.exception.PostResult;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -14,15 +13,29 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    List<User> userList = new ArrayList<>();
+    List<User> userLists = initUserList();
+
+    private List<User> initUserList() {
+        List<User> userTempList = new ArrayList<>();
+        userTempList.add(new User("qq", "femail", 20, "123@123.com", "10123456789"));
+        return userTempList;
+    }
 
     @PostMapping("/user/register")
-    public ResponseEntity<PostResult> registUser(@Valid @RequestBody User user){
-        userList.add(user);
+    public ResponseEntity<PostResult> registUser(@Valid @RequestBody User user) {
+        userLists.add(user);
         PostResult postResult = new PostResult();
-        int num = userList.indexOf(user);
+        int num = userLists.indexOf(user);
         postResult.setIndex(num);
         return ResponseEntity.created(null).body(postResult);
+    }
+
+    @GetMapping("/rs/userList")
+    public ResponseEntity<List<User>> getRsEventByRange() {
+//        if (start == null || end == null) {
+//            return ResponseEntity.created(null).body(userList);
+//        }
+        return ResponseEntity.created(null).body(userLists);
     }
 
 }
--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -11,8 +11,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -24,8 +27,20 @@ class UserControllerTest {
     MockMvc mockMvc;
 
     @Test
+    void contextLoads() throws Exception {
+        mockMvc.perform(get("/rs/userList"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$[0].name", is("qq")))
+                .andExpect(jsonPath("$[0].gender", is("femail")))
+                .andExpect(jsonPath("$[0].age", is(20)))
+                .andExpect(jsonPath("$[0].email", is("123@123.com")))
+                .andExpect(jsonPath("$[0].phone", is("10123456789")));
+
+    }
+
+    @Test
     void user_register() throws Exception {
-        User user = new User("qqq","男",19,"994059641@qq.com","10123456789");
+        User user = new User("qqq", "男", 19, "994059641@qq.com", "10123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
@@ -37,7 +52,7 @@ class UserControllerTest {
 
     @Test
     void user_name_not_mull() throws Exception {
-        User user = new User("","男",19,"123@123.com","10123456789");
+        User user = new User("", "男", 19, "123@123.com", "10123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
@@ -49,7 +64,7 @@ class UserControllerTest {
 
     @Test
     void user_name_not_more_8() throws Exception {
-        User user = new User("123456789","男",19,"123@123.com","10123456789");
+        User user = new User("123456789", "男", 19, "123@123.com", "10123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
@@ -61,7 +76,7 @@ class UserControllerTest {
 
     @Test
     void user_gender_not_null() throws Exception {
-        User user = new User("12345678","",19,"123@123.com","10123456789");
+        User user = new User("12345678", "", 19, "123@123.com", "10123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
@@ -73,7 +88,7 @@ class UserControllerTest {
 
     @Test
     void user_age_not_null() throws Exception {
-        User user = new User("12345678","男", null,"123@123.com","10123456789");
+        User user = new User("12345678", "男", null, "123@123.com", "10123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
@@ -85,7 +100,7 @@ class UserControllerTest {
 
     @Test
     void user_age_18_more() throws Exception {
-        User user = new User("qqq","男",17,"123@123.com","10123456789");
+        User user = new User("qqq", "男", 17, "123@123.com", "10123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
@@ -97,7 +112,7 @@ class UserControllerTest {
 
     @Test
     void user_age_100_less() throws Exception {
-        User user = new User("qqq","男",117,"123@123.com","10123456789");
+        User user = new User("qqq", "男", 117, "123@123.com", "10123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
@@ -109,7 +124,7 @@ class UserControllerTest {
 
     @Test
     void user_email_valid() throws Exception {
-        User user = new User("qqq","男",19,"1123.com","10123456789");
+        User user = new User("qqq", "男", 19, "1123.com", "10123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
@@ -121,7 +136,7 @@ class UserControllerTest {
 
     @Test
     void user_phone_number_not_start_1() throws Exception {
-        User user = new User("qqq","男",19,"11@23.com","20123456789");
+        User user = new User("qqq", "男", 19, "11@23.com", "20123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
@@ -133,7 +148,7 @@ class UserControllerTest {
 
     @Test
     void user_phone_number_not_length_is_11() throws Exception {
-        User user = new User("qqq","男",19,"11@23.com","1123456789");
+        User user = new User("qqq", "男", 19, "11@23.com", "1123456789");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonUserStr = objectMapper.writeValueAsString(user);
 
